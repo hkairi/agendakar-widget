9bb0550da262d0c5a6d4cba89dd1cade252047ce
Hassane Moustapha <hassanemoustapha@gmail.com>
2014-12-23 14:44:19 +0000

    trying 2 versions of the code

--- README.md
+++
@@ -1,1 +0,0 @@
-  Agendakar React Widget
---
+++ js/src/agendakar.js
@@ -0,0 +1,60 @@
+var React;
+var Header= React.createClass({
+  render: function(){
+    return( <div> <h1>Agendakar</h1> </div>);
+  }
+});
+
+var Evenement = React.createClass({
+  render: function(){
+    return(
+      <li>
+        <a href='#'> titre </a>
+      </li>
+    );
+  }
+});
+
+var Liste= React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1>Liste</h1>
+        <Evenement />
+        <Evenement />
+        <Evenement />
+        <Evenement />
+      </div>
+    );
+  }
+});
+
+var Footer= React.createClass({
+  render: function(){
+    return( <div> <h1>Footer</h1> </div>);
+  }
+});
+
+var AgendakarWidget= React.createClass({
+  displayName: 'agendakar-widget',
+
+  render: function(){
+    var styles = {
+      width: '300px',
+      height: '400px',
+      border: '1px solid #A3ABAC',
+      float: 'right'
+    };
+    return(
+      <div style={styles}>
+        <Header />
+        <Liste />
+        <Footer />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <AgendakarWidget />, document.getElementById('content')
+);
