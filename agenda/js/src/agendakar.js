478195eb62ec0aab47e1be45abf578cf8857240b
Hassane Moustapha <hassanemoustapha@gmail.com>
2015-01-02 11:24:30 +0000

    updated the main widget file

--- agenda/js/src/agendakar.js
+++ agenda/js/src/agendakar.js
@@ -1,9 +1,9 @@
 var AgendakarWidget = require('./components/agendakar');
 
 var e   = document.getElementById('agendakar-widget');
-var d   = e.attributes['el'].value;
-var _id = e.attributes['cid'].value;
-var c   = e.attributes['cats'].value.split(",");
+var d   = e.attributes['data-el'].value;
+var _id = e.attributes['data-cid'].value;
+var c   = e.attributes['data-cats'].value.split(",");
 
 React.render(
   <AgendakarWidget clientId={_id} categories={c}/>, document.getElementById(d)
